f8977b8aec60a52d0e501a52d5ede274c8b958bd
David <gw.chip.gw@gmail.com>
2018-12-05 15:58:53 -0600

    Tweaks to the client mod script. It now passes a context string and the player name on connect. (And doesn't pass the name every frame).

--- minetestmod/mumble/init.lua
+++ minetestmod/mumble/init.lua
@@ -2,11 +2,12 @@ local player
 
 minetest.register_on_connect(function()
     player = minetest.localplayer
+    print("mumble id "..player:get_name())
+    print("mumble context "..minetest.get_server_info().ip..":"..minetest.get_server_info().port)
 end)
 
 minetest.register_globalstep(function(dtime)
     if player then
-        local player_name = player:get_name()
         local player_pos = player:get_pos() or {x=0, y=0, z=0}
         local player_look = {x=0, y=0, z=0}
         local camera_pos = {x=0, y=0, z=0}
@@ -19,7 +20,6 @@ minetest.register_globalstep(function(dtime)
             camera_look = minetest.camera:get_look_dir()
             player_look = camera_look
         end
-        print("mumble id "..player_name)
         print("p p ["..(player_pos.x).." "..(player_pos.y).." "..(player_pos.z).."]")
         print("p l ["..(player_look.x).." "..(player_look.y).." "..(player_look.z).."]")
         print("c p ["..(camera_pos.x).." "..(camera_pos.y).." "..(camera_pos.z).."]")
